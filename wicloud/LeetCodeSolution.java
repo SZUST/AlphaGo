c71f1463daf7be3e20e295b428bf94e2d2ec0888
wicloud <w6394663@gmail.com>
2016-05-06 00:31:06 +0800

    2. Add Two Numbers
    Your runtime beats 35.18% of javasubmissions.

--- wicloud/LeetCodeSolution.java
+++ wicloud/LeetCodeSolution.java
@@ -9,6 +9,80 @@ import java.util.Arrays;
 public class Solution {
 
 	/**
+	* 
+	* 2. Add Two Numbers   Total Accepted: 138344 Total Submissions: 602371 Difficulty: Medium
+	* 
+	* You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+	* Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+	* Output: 7 -> 0 -> 8
+	* 
+	* @param l1
+	* @param l2
+	* @return
+	*/
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		int up = 0;
+		ListNode res = l1;
+		int val = 0;
+		while (null != l1) {
+			val = (l1.val + l2.val + up) % 10;
+			up = (l1.val + l2.val + up) / 10;
+			l1.val = val;
+			if (null == l1.next) { //指向l2
+				l1.next = l2.next;
+				if (l1.next == null && up > 0) {
+					l1.next = new ListNode(up);
+					break;
+				}
+				l1 = l1.next;
+				l2.next = null;
+				l2.val = 0;
+				continue;
+			}
+			if (null == l2.next) {
+				l1 = l1.next;
+				l2.val = 0;
+			} else {
+				l1 = l1.next;
+				l2 = l2.next;
+			}
+		}
+		return res;
+	}
+
+	//	  Definition for singly-linked list.
+	public class ListNode {
+
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	/**
+	 *
+	 * 1. Two Sum    O(n*n)  O(1)          -.>建议使用hashMap
+	 * 
+	 *  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+	 *	You may assume that each input would have exactly one solution.
+	 * @author  wicloud
+	 * @date   2016-4-24
+	 */
+	public int[] twoSum(int[] nums, int target) {
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		return new int[] { -1, -1 };
+	}
+
+	/**
 	 * 16.  3Sum Closest 
 	 * 
 	 * Closest Given an array S of n integers, find three integers in S such that the sum is closest to a given
