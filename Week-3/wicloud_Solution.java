0b1d61e6f7a140d39cea998e4fb0de41c4591ca8
wicloud <w6394663@gmail.com>
2016-05-11 23:02:44 +0800

    13 Roman to Integer 39.5% Easy
    知道了罗马数字和interger之间的转换就不太难了

--- Week-3/wicloud_Solution.java
+++ Week-3/wicloud_Solution.java
@@ -25,12 +25,50 @@ public class Solution3 {
 	 *  Given a roman numeral, convert it to an integer.
 	 *  Input is guaranteed to be within the range from 1 to 3999.
 	 *  罗马数字中I=1，V=5，X=10，L=50，C=100，D=500，M=1000
-	 * @param s
-	 * @return 
+	 *  @param s
+	 *  @return 
+	 * 
+		3999 / 3999 test cases passed.
+		Status: Accepted
+		Runtime: 8 ms
+	    Your runtime beats 67.32% of javasubmissions
 	 */
 
 	public int romanToInt(String s) {
+		if (0 == s.length()) {
+			return 0;
+		}
+		char[] arr = s.toCharArray();
+		int res = toInt(arr[arr.length - 1]);
+		for (int i = arr.length - 1; i != 0; i--) {
+			if (toInt(arr[i]) > toInt(arr[i - 1])) {
+				res -= toInt(arr[i - 1]);
+			} else {
+				res += toInt(arr[i - 1]);
+			}
+		}
+		return res;
+	}
 
+	private int toInt(char c) {
+		switch (c) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			return 0;
+		}
 	}
 
 }
