ddd20b581aaf748845b9c3494801e3e3dbbe3001
wicloud <w6394663@gmail.com>
2016-05-11 22:22:40 +0800

    Create wicloud_Solution.java

---
+++ Week-3/wicloud_Solution.java
@@ -0,0 +1,36 @@
+/**
+ * @(#)Solution3.java   2016-5-11
+ * Copyright 2016  wicloud.com, Inc. All rights reserved.
+ */
+
+package com.wicloud.alphago;
+
+/**
+ * Week-3 LeetCode 习题
+ *
+	3 Longest Substring Without Repeating Characters 22.1% Medium
+	4 Median of Two Sorted Arrays 18.7% Hard
+	5 Longest Palindromic Substring
+	13 Roman to Integer 39.5% Easy
+	14 Longest Common Prefix
+ *
+ * @author  wicloud
+ * @date   2016-5-11
+ */
+
+public class Solution3 {
+
+	/**
+	 *  13. Roman to Integer
+	 *  Given a roman numeral, convert it to an integer.
+	 *  Input is guaranteed to be within the range from 1 to 3999.
+	 *  罗马数字中I=1，V=5，X=10，L=50，C=100，D=500，M=1000
+	 * @param s
+	 * @return 
+	 */
+
+	public int romanToInt(String s) {
+
+	}
+
+}
